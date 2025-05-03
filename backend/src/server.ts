8ebbf42c4f9c1b13f2b2bc9326ffff82f0e2ddf7
shuklatul1021 <165987183+shuklatul1021@users.noreply.github.com>
2025-05-03 15:28:52 +0530

    addbackend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,7 +7,7 @@ import cors from "cors"
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'https://coursemania.codecollabhub.xyz/', 
   credentials: true
 }));
 
